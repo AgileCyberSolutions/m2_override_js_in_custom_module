651821e79397015a0a2b12e25a221e293ff3812c
Agile Cyber Management Solutions <support@agilecyber.com>
2023-10-04 18:23:31 +0530

    Override Js files in Custom Module

---
+++ Agile/CustomerAddress/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    map: {
+        '*': {
+            
+            'Magento_Customer/js/model/customer-addresses': 'Agile_CustomerAddress/js/model/customer-addresses'
+
+        },
+    }
+};
